cbb1574e812d0130b50b8b1fae5cc0e7a3508bb8
hyeonukdev <lego7407@gmail.com>
2020-06-27 00:27:26 +0900

    #4-2020-06-27

---
+++ #4_함수.js
@@ -0,0 +1,22 @@
+// 함수
+// 프로그래머가 임의로 정한 코드의 실행 단위
+// 코드의 동작 단위
+
+// 짧고 간결하게 하나의 기능만을 잘 수행하는 코드
+
+function 함수 이름 (인자, 인자, ...) {
+    // 함수 동작 코드
+
+    return 리턴값
+}
+
+function sayHello(name){
+    hi = 'hello' + name + "!";
+    return hi;
+}
+sayHello('길동');
+
+function addNumber (num1, num2){
+    return num1 + num2;
+}
+addNumber(2, 4);
